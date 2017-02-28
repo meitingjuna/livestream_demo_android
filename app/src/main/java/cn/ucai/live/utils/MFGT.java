0fe2afbb6263ae2896366f9cb065927cb231689a
meitingjun <1271888057@qq.com>
2017-02-28 10:33:01 +0800

    添加工具类

---
+++ app/src/main/java/cn/ucai/live/utils/MFGT.java
@@ -0,0 +1,75 @@
+package cn.ucai.live.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+
+import cn.ucai.live.I;
+import cn.ucai.live.R;
+import cn.ucai.live.ui.activity.LoginActivity;
+import cn.ucai.live.ui.activity.MainActivity;
+import cn.ucai.live.ui.activity.RegisterActivity;
+
+
+/**
+ * Created by MTJ on 2017/1/10.
+ */
+
+public class MFGT {
+    public static void finish(Activity activity) {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    public static void startActivity(Activity context, Class<?> clz) {
+        context.overridePendingTransition(R.anim.push_right_in, R.anim.push_bottom_out);
+        context.startActivity(new Intent(context, clz));
+
+    }
+
+    public static void startActivity(Activity context, Intent intent) {
+        context.overridePendingTransition(R.anim.push_right_in, R.anim.push_bottom_out);
+        context.startActivity(intent);
+    }
+
+    public static void gotoLogin(Activity activity) {
+        startActivity(activity, LoginActivity.class);
+    }
+
+    public static void gotoLoginCleanTask(Activity activity) {
+        startActivity(activity, new Intent(activity, LoginActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    public static void gotoRegister(Activity activity) {
+        startActivity(activity, RegisterActivity.class);
+    }
+
+
+
+    public static void gotoMiain(Activity activity) {
+        startActivity(activity, new Intent(activity, MainActivity.class).
+                putExtra(I.BACK_MAIN_FROM_CHAT, true));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
