64e812a27da452bb7e341af2c2cee211e36e6318
meitingjun <1271888057@qq.com>
2017-02-28 16:34:18 +0800

    用户注册添加服务器的代码

--- app/src/main/java/cn/ucai/live/ui/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/LoginActivity.java
@@ -86,6 +86,9 @@ public class LoginActivity extends BaseActivity {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
+        if (LiveHelper.getInstance().getCurrentUsernName()!=null){
+            mEmailView.setText(LiveHelper.getInstance().getCurrentUsernName());
+        }
 
 
     }
--- app/src/main/java/cn/ucai/live/ui/activity/RegisterActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/RegisterActivity.java
@@ -5,27 +5,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import cn.ucai.live.I;
+import cn.ucai.live.LiveHelper;
 import cn.ucai.live.R;
+
+import com.hyphenate.EMError;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.exceptions.HyphenateException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.ucai.live.data.NetDao;
+import cn.ucai.live.data.model.Result;
+import cn.ucai.live.utils.CommonUtils;
+import cn.ucai.live.utils.MD5;
+import cn.ucai.live.utils.OnCompleteListener;
+import cn.ucai.live.utils.ResultUtils;
 
 public class RegisterActivity extends BaseActivity {
-
+    //用户名
     @BindView(R.id.email)
-    EditText username;
+    EditText etUsername;
+    //昵称
+    @BindView(R.id.usernick)
+    EditText etUsernick;
+    //密码
     @BindView(R.id.password)
-    EditText password;
+    EditText etPassword;
+    //确认密码
+    @BindView(R.id.password_confirm)
+    EditText etPasswordConfirm;
+    //
     @BindView(R.id.register)
     Button register;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    String username, usernick, pasword;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,46 +60,137 @@ public class RegisterActivity extends BaseActivity {
                 finish();
             }
         });
-
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(TextUtils.isEmpty(username.getText()) || TextUtils.isEmpty(password.getText())){
-                    showToast("用户名和密码不能为空");
+                String confirm_pwd = etPasswordConfirm.getText().toString().trim();
+                username = etUsername.getText().toString().trim();
+                usernick = etUsernick.getText().toString().trim();
+                pasword = etPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(username)) {
+                    CommonUtils.showShortToast(R.string.User_name_cannot_be_empty);
+                    etUsername.requestFocus();
+                    return;
+                } else if (TextUtils.isEmpty(pasword)) {
+                    CommonUtils.showShortToast(R.string.Password_cannot_be_empty);
+                    etPassword.requestFocus();
+                    return;
+                } else if (TextUtils.isEmpty(usernick)) {
+                    CommonUtils.showShortToast(R.string.User_nick_cannot_be_empty);
+                    etUsernick.requestFocus();
+                    return;
+                } else if (TextUtils.isEmpty(confirm_pwd)) {
+                    CommonUtils.showShortToast(R.string.Confirm_password_cannot_be_empty);
+                    etPasswordConfirm.requestFocus();
+                    return;
+                } else if (!pasword.equals(confirm_pwd)) {
+                    CommonUtils.showShortToast(R.string.Two_input_password);
                     return;
                 }
-                final ProgressDialog pd = new ProgressDialog(RegisterActivity.this);
-                pd.setMessage("正在注册...");
-                pd.setCanceledOnTouchOutside(false);
-                pd.show();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            EMClient.getInstance().createAccount(username.getText().toString(), password.getText().toString());
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    pd.dismiss();
-                                    showToast("注册成功");
-                                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                                    finish();
-                                }
-                            });
-                        } catch (final HyphenateException e) {
-                            e.printStackTrace();
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    pd.dismiss();
-                                    showLongToast("注册失败：" + e.getMessage());
-                                }
-                            });
-                        }
-                    }
-                }).start();
-
+                if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(pasword)) {
+                    pd = new ProgressDialog(RegisterActivity.this);
+                    pd.setMessage(getResources().getString(R.string.Is_the_registered));
+                    registerAppServer();
+                }
             }
         });
     }
+
+    private void registerAppServer() {
+        //注册自己服务器账号
+        NetDao.register(this, username, usernick, pasword, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                if (s != null) {
+                    Result result = ResultUtils.getResultFromJson(s, null);
+                    if (result != null) {
+                        if (result.isRetMsg()) {
+                            //注册成功后调用环信注册
+                            registerEMServer();
+                        } else {
+                            pd.dismiss();
+                            if (result.getRetCode() == I.MSG_REGISTER_USERNAME_EXISTS) {
+                                CommonUtils.showLongToast(R.string.User_already_exists);
+                            } else {
+                                CommonUtils.showLongToast(R.string.Registration_failed);
+                            }
+                        }
+                    } else {
+                        pd.dismiss();
+                        CommonUtils.showLongToast(R.string.Registration_failed);
+                    }
+                } else {
+                    pd.dismiss();
+                    CommonUtils.showLongToast(R.string.Registration_failed);
+                }
+
+            }
+
+            @Override
+            public void onError(String error) {
+                pd.dismiss();
+                CommonUtils.showLongToast(R.string.Registration_failed);
+            }
+        });
+    }
+
+    private void registerEMServer() {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    // call method in SDK
+                    EMClient.getInstance().createAccount(username, MD5.getMessageDigest(pasword));
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            if (!RegisterActivity.this.isFinishing())
+                                pd.dismiss();
+                            // save current user
+                            LiveHelper.getInstance().setCurrentUserName(username);
+                            showToast(getResources().getString(R.string.Registered_successfully));
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                            finish();
+                        }
+                    });
+                } catch (final HyphenateException e) {
+                    unRegisterAppSever();
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            if (!RegisterActivity.this.isFinishing())
+                                pd.dismiss();
+                            int errorCode = e.getErrorCode();
+                            if (errorCode == EMError.NETWORK_ERROR) {
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.network_anomalies), Toast.LENGTH_SHORT).show();
+                            } else if (errorCode == EMError.USER_ALREADY_EXIST) {
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.User_already_exists), Toast.LENGTH_SHORT).show();
+                            } else if (errorCode == EMError.USER_AUTHENTICATION_FAILED) {
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.registration_failed_without_permission), Toast.LENGTH_SHORT).show();
+                            } else if (errorCode == EMError.USER_ILLEGAL_ARGUMENT) {
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.illegal_user_name), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registration_failed), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private void unRegisterAppSever() {
+        NetDao.unregister(this, username, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.e("RegisterActivity", "result=" + result);
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+
+    }
+
+
 }
